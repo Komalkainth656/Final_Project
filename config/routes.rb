26239d90b4553685819a15ecbb7d0d4700261774
komal <kainthkomal656@gmail.com>
2020-08-23 16:29:42 -0700

    added authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :recipes do
-    resources :comments
+   resources :users, only: [:new, :create]
+   resources :sessions, only: [:new, :create] do
+    delete :destroy, on: :collection
   end
+   resources :recipes do
+   resources :comments
+   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_23_224023) do
+ActiveRecord::Schema.define(version: 2020_08_23_225400) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,15 @@ ActiveRecord::Schema.define(version: 2020_08_23_224023) do
   create_table "recipes", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
